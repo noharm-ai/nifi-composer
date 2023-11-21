cf557e3dc38f97c1429dc95e8746914de4164ae2
Henrique Dias <heukirne@gmail.com>
2023-11-21 14:03:48 -0300

    Update genkeypair.sh

--- genkeypair.sh
+++ genkeypair.sh
@@ -16,6 +16,10 @@ prop_replace () {
   sed -i -e "s|^$1=.*$|$1=$2|"  ${target_file}
 }
 
+#Path to Keytool
+KEYTOOL_HOME=$(readlink -f $(which keytool))
+echo KEYTOOL_HOME=${KEYTOOL_HOME}
+
 # Paths to NiFi properties file
 nifi_props_file=${NIFI_HOME}/conf/nifi.properties
 
@@ -36,25 +40,25 @@ echo Generating KeyStore...
 
 #openssl pkcs12 -export -out ${NIFI_HOME}/conf/new_keystore.p12 -inkey ${NIFI_HOME}/conf/key.pem -in ${NIFI_HOME}/conf/cert.pem -name nifi-key -passin pass:${KEYSTORE_PASSWORD} -passout pass:${KEYSTORE_PASSWORD}
 
-/opt/java/openjdk/bin/keytool -genkeypair -keystore ${NIFI_HOME}/conf/new_keystore.p12 -storetype PKCS12 -storepass ${KEYSTORE_PASSWORD} -alias nifi-key -keyalg RSA -keysize 2048 -validity 99999 -dname "CN=localhost" -ext san=dns:localhost,dns:noharm-nifi -ext bc=ca:true -ext eku=serverAuth,clientAuth -ext ku=digitalSignature,nonRepudiation,keyEncipherment,dataEncipherment,keyAgreement,keyCertSign,cRLSign
+${KEYTOOL_HOME} -genkeypair -keystore ${NIFI_HOME}/conf/new_keystore.p12 -storetype PKCS12 -storepass ${KEYSTORE_PASSWORD} -alias nifi-key -keyalg RSA -keysize 2048 -validity 99999 -dname "CN=localhost" -ext san=dns:localhost,dns:noharm-nifi -ext bc=ca:true -ext eku=serverAuth,clientAuth -ext ku=digitalSignature,nonRepudiation,keyEncipherment,dataEncipherment,keyAgreement,keyCertSign,cRLSign
 
-/opt/java/openjdk/bin/keytool -v -list -keystore ${NIFI_HOME}/conf/new_keystore.p12 -storepass ${KEYSTORE_PASSWORD}
+${KEYTOOL_HOME} -v -list -keystore ${NIFI_HOME}/conf/new_keystore.p12 -storepass ${KEYSTORE_PASSWORD}
 
 echo Extracting Certificate...
 [ -f "${NIFI_HOME}/conf/cert.crt" ] && rm ${NIFI_HOME}/conf/cert.crt
-/opt/java/openjdk/bin/keytool -exportcert -keystore ${NIFI_HOME}/conf/keystore.p12 -storepass ${KEYSTORE_PASSWORD} -alias nifi-key -rfc -file ${NIFI_HOME}/conf/cert.crt
+${KEYTOOL_HOME} -exportcert -keystore ${NIFI_HOME}/conf/keystore.p12 -storepass ${KEYSTORE_PASSWORD} -alias nifi-key -rfc -file ${NIFI_HOME}/conf/cert.crt
 
-/opt/java/openjdk/bin/keytool -printcert -v -file ${NIFI_HOME}/conf/cert.crt
+${KEYTOOL_HOME} -printcert -v -file ${NIFI_HOME}/conf/cert.crt
 
-/opt/java/openjdk/bin/keytool -importcert -file ${NIFI_HOME}/conf/cert.crt -alias nifi-new-key -keystore ${NIFI_HOME}/conf/new_keystore.p12 -storetype PKCS12 -storepass ${KEYSTORE_PASSWORD} -noprompt
+${KEYTOOL_HOME} -importcert -file ${NIFI_HOME}/conf/cert.crt -alias nifi-new-key -keystore ${NIFI_HOME}/conf/new_keystore.p12 -storetype PKCS12 -storepass ${KEYSTORE_PASSWORD} -noprompt
 
-/opt/java/openjdk/bin/keytool -v -list -keystore ${NIFI_HOME}/conf/new_keystore.p12 -storepass ${KEYSTORE_PASSWORD}
+${KEYTOOL_HOME} -v -list -keystore ${NIFI_HOME}/conf/new_keystore.p12 -storepass ${KEYSTORE_PASSWORD}
 
 echo Generating TrustKeyStore...
 [ -f "${NIFI_HOME}/conf/new_truststore.p12" ] && rm ${NIFI_HOME}/conf/new_truststore.p12
-/opt/java/openjdk/bin/keytool -import -file ${NIFI_HOME}/conf/cert.crt -alias nifi-cert -keystore ${NIFI_HOME}/conf/new_truststore.p12 -storetype PKCS12 -keypass ${TRUSTSTORE_PASSWORD} -storepass ${TRUSTSTORE_PASSWORD} -noprompt
+${KEYTOOL_HOME} -import -file ${NIFI_HOME}/conf/cert.crt -alias nifi-cert -keystore ${NIFI_HOME}/conf/new_truststore.p12 -storetype PKCS12 -keypass ${TRUSTSTORE_PASSWORD} -storepass ${TRUSTSTORE_PASSWORD} -noprompt
 
-/opt/java/openjdk/bin/keytool -v -list -keystore ${NIFI_HOME}/conf/new_truststore.p12 -storepass ${TRUSTSTORE_PASSWORD}
+${KEYTOOL_HOME} -v -list -keystore ${NIFI_HOME}/conf/new_truststore.p12 -storepass ${TRUSTSTORE_PASSWORD}
 
 prop_replace 'nifi.security.keystore' "${NIFI_HOME}/conf/new_keystore.p12"
 prop_replace 'nifi.security.truststore' "${NIFI_HOME}/conf/new_truststore.p12"
