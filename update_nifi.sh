7fc09499c1e23740d81d44fc09c6b21236674143
Henrique Dias <heukirne@gmail.com>
2024-05-24 15:19:45 -0300

    Update update_nifi.sh

--- update_nifi.sh
+++ update_nifi.sh
@@ -6,3 +6,5 @@ wget -c https://repo1.maven.org/maven2/com/oracle/database/jdbc/ojdbc8/23.2.0.0/
 wget -c https://jdbc.postgresql.org/download/postgresql-42.6.0.jar -P ${NIFI_HOME}/lib
 wget -c https://repo1.maven.org/maven2/org/apache/nifi/nifi-kite-nar/1.15.3/nifi-kite-nar-1.15.3.nar -P ${NIFI_HOME}/lib
 echo "java.arg.8=-Duser.timezone=America/Sao_Paulo" >> ${NIFI_HOME}/conf/bootstrap.conf
+sed -i 's/^java.arg.2=.*/java.arg.2=-Xms1024m/' ${NIFI_HOME}/conf/bootstrap.conf
+sed -i 's/^java.arg.3=.*/java.arg.3=-Xms1024m/' ${NIFI_HOME}/conf/bootstrap.conf
