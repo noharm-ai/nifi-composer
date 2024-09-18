78bfbd2c2e889463bbc94d499d28ca6a3b63fe2c
Olimar Teixeira Borges <olimarbjunior@gmail.com>
2024-09-18 17:58:14 -0300

    abordagem condicional para buscar o valor de memória disponível de duas formas diferentes, para quando o valor não vier de uma forma, tentar buscar de outra forma.

--- script_integ_01.sh
+++ script_integ_01.sh
@@ -68,9 +68,17 @@ validate_requirements() {
 
     DISK_TOTAL=$(df -h / | awk 'NR==2 {print $2}' | tr -d 'G')  # Captura o espaço total em GB
     DISK_AVAILABLE=$(df -h / | awk 'NR==2 {print $4}' | tr -d 'G')  # Captura o espaço livre em GB
-    MEM_TOTAL=$(free -m | awk '/^Mem.:/{print $2}')  # Captura a memória total em MB
+    MEM_TOTAL_1=$(free -m | awk '/^Mem.:/{print $2}')  # Captura a memória total em MB
+    MEM_TOTAL_2=$(free -m | awk '/^Mem:/{print $2}')  # Captura a memória total em MB
     MEM_AVAILABLE=$(get_memory_available)  # Captura a memória disponível em MB
     VCPUS=$(nproc)  # Captura o número de vCPUs disponíveis
+
+    # Verifica se MEM_TOTAL_1 está vazio. Se estiver, usa MEM_TOTAL_2. Caso contrário, usa MEM_TOTAL_1.
+    if [ -z "$MEM_TOTAL_1" ]; then
+        MEM_TOTAL=$MEM_TOTAL_2
+    else
+        MEM_TOTAL=$MEM_TOTAL_1
+    fi
 
     echo "Espaço total em disco: ${DISK_TOTAL}GB"
     echo "Espaço disponível em disco: ${DISK_AVAILABLE}GB"
