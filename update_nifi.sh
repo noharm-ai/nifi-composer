630212c3a379c18175e2ffb3829abb719efd5390
Júlia Colleoni Couto <34305916+juliacolleoni@users.noreply.github.com>
2024-10-10 09:17:29 -0300

    Update driver postgre

--- update_nifi.sh
+++ update_nifi.sh
@@ -4,7 +4,7 @@ wget -c https://truststore.pki.rds.amazonaws.com/sa-east-1/sa-east-1-bundle.pem 
 wget -c https://repo1.maven.org/maven2/mysql/mysql-connector-java/8.0.30/mysql-connector-java-8.0.30.jar -P ${NIFI_HOME}/lib
 wget -c https://repo1.maven.org/maven2/com/oracle/database/jdbc/ojdbc8/23.2.0.0/ojdbc8-23.2.0.0.jar -P ${NIFI_HOME}/lib
 wget -c https://repo1.maven.org/maven2/com/microsoft/sqlserver/mssql-jdbc/12.6.2.jre11/mssql-jdbc-12.6.2.jre11.jar -P ${NIFI_HOME}/lib
-wget -c https://jdbc.postgresql.org/download/postgresql-42.6.0.jar -P ${NIFI_HOME}/lib
+wget -c https://jdbc.postgresql.org/download/postgresql-42.7.3.jar -P ${NIFI_HOME}/lib
 wget -c https://repo1.maven.org/maven2/org/apache/nifi/nifi-kite-nar/1.15.3/nifi-kite-nar-1.15.3.nar -P ${NIFI_HOME}/lib
 echo "java.arg.8=-Duser.timezone=America/Sao_Paulo" >> ${NIFI_HOME}/conf/bootstrap.conf
 sed -i 's/^nifi.provenance.repository.max.storage.time=.*/nifi.provenance.repository.max.storage.time=3 days/' ${NIFI_HOME}/conf/nifi.properties
