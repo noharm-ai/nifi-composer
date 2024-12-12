15fd5c1393b5d193682f56af73d4cf5a1d8b5e20
Olimar T. Borges <olimarbjunior@gmail.com>
2024-12-12 08:32:51 -0300

    ajuste do crontab (para execução a cada 1 hora)ao script de cópia dos arquivos .json.gz e .xml.gz para o bucket da S3

--- script_backup_nifi.sh
+++ script_backup_nifi.sh
@@ -1,37 +1,13 @@
 #!/bin/bash
 
-# Caminho para o diretório base
-BASE_DIR="/nifi-composer"
-SCRIPT_DIR="$BASE_DIR/nifi-scripts"
-
-# Garantir que a pasta nifi-scripts exista
-if [ ! -d "$SCRIPT_DIR" ]; then
-  echo "Criando o diretório $SCRIPT_DIR..."
-  mkdir -p "$SCRIPT_DIR"
-  chmod 755 "$SCRIPT_DIR"
-fi
-
-# Caminho para o arquivo noharm.env
-ENV_FILE="$BASE_DIR/noharm.env"
-
 # Função para verificar ou configurar parâmetros
 configure_param() {
   local param_name="$1"
   local param_value="$2"
 
-  if [ -n "$param_value" ]; then
-    # Adicionar ao arquivo noharm.env, se não existir
-    if ! grep -q "^${param_name}=" "$ENV_FILE"; then
-      echo "$param_name=$param_value" >> "$ENV_FILE"
-    fi
-  else
-    # Buscar no arquivo noharm.env
-    if grep -q "^${param_name}=" "$ENV_FILE"; then
-      param_value=$(grep "^${param_name}=" "$ENV_FILE" | cut -d'=' -f2-)
-    else
-      echo "Erro: $param_name não foi passado e não está configurado no $ENV_FILE."
-      exit 1
-    fi
+  if [ -z "$param_value" ]; then
+    echo "Erro: $param_name não foi passado como argumento."
+    exit 1
   fi
 
   echo "$param_value"
@@ -61,19 +37,6 @@ SERVICO_NIFI=$(configure_param "SERVICO_NIFI" "$SERVICO_NIFI") || exit 1
 
 # Configurar S3_BUCKET_PATH fixo
 S3_BUCKET_PATH="s3://noharm-nifi"
-if ! grep -q "^S3_BUCKET_PATH=" "$ENV_FILE"; then
-  echo "S3_BUCKET_PATH=$S3_BUCKET_PATH" >> "$ENV_FILE"
-fi
-
-# Configurar credenciais AWS
-export AWS_ACCESS_KEY_ID=$(grep "^AWS_ACCESS_KEY_ID=" "$ENV_FILE" | cut -d'=' -f2-)
-export AWS_SECRET_ACCESS_KEY=$(grep "^AWS_SECRET_ACCESS_KEY=" "$ENV_FILE" | cut -d'=' -f2-)
-export AWS_DEFAULT_REGION=$(grep "^AWS_DEFAULT_REGION=" "$ENV_FILE" | cut -d'=' -f2-)
-
-if [ -z "$AWS_ACCESS_KEY_ID" ] || [ -z "$AWS_SECRET_ACCESS_KEY" ] || [ -z "$AWS_DEFAULT_REGION" ]; then
-  echo "Erro: Credenciais AWS não configuradas no arquivo $ENV_FILE."
-  exit 1
-fi
 
 # Exibir os valores configurados para depuração
 echo "### Cliente: $NOME_DO_CLIENTE"
@@ -113,6 +76,6 @@ fi
 "
 
 # Adicionar um crontab para execução automática
-CRON_JOB="0 * * * * bash <(curl -s https://raw.githubusercontent.com/noharm-ai/nifi-composer/main/script_backup_nifi.sh) --cliente $NOME_DO_CLIENTE --servico $SERVICO_NIFI"
+CRON_JOB="0 * * * * curl -s https://raw.githubusercontent.com/noharm-ai/nifi-composer/main/script_backup_nifi.sh | bash --cliente $NOME_DO_CLIENTE --servico $SERVICO_NIFI"
 (crontab -l 2>/dev/null | grep -v "script_backup_nifi.sh" ; echo "$CRON_JOB") | crontab -
 echo "Crontab configurado para executar o script a cada 1 hora."
