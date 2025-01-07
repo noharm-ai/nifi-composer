3e5d296cd28a293b4d6458dbf2b0fd19a91dee2c
Olimar Teixeira Borges <olimarbjunior@gmail.com>
2025-01-07 12:55:35 -0300

    Tentativa de utilização de script no gihut para execução de processo dentro do Nifi

--- nifi_create_backup.sh
+++ nifi_create_backup.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-schema_name=$1
+schema_name=$1  # Captura o primeiro argumento passado pelo bash
+echo "Usando schema_name: $schema_name"
 
 mkdir -p /tmp/nifi-upload && \
 find /opt/nifi/nifi-current/conf -maxdepth 1 -type f \( -name 'flow.json.gz' -o -name 'flow.xml.gz' -o -name 'nifi.properties' \) -exec cp {} /tmp/nifi-upload/ \; && \
